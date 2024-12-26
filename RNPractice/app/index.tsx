8402f49737c845019699935073213142fe07f84e
lee juyoung <juyung0903@naver.com>
2024-12-26 17:46:17 +0900

    reset project + ~섹션1 FlatList

---
+++ RNPractice/app/index.tsx
@@ -0,0 +1,98 @@
+import { StatusBar } from "expo-status-bar";
+import { SetStateAction, useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ScrollView,
+  FlatList,
+} from "react-native";
+
+interface courseGoal {
+  text:string;
+  id:string;
+}
+export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState<string>("");
+  const [courseGoals, setCourseGoals] = useState<courseGoal[]>([]);
+  function goalInputHandler(enteredText: string) {
+    setEnteredGoalText(enteredText);
+  }
+  function addGoalHandler() {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      { text: enteredGoalText, id: Math.random().toString() },
+    ]);
+  }
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          onChangeText={goalInputHandler}
+          style={styles.textInput}
+          placeholder="Your course goal!"
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
+      </View>
+      <View style={styles.goalsContainer}>
+        {/* view에 안하고 text에 적용하면 ios에 둥근모서리 적용x */}
+        {/* <ScrollView> */}
+        {/* FlatList는 좀더최적화에유리(화면에보여야 로딩 및 렌더링)
+        FlatList가 key를 자동적으로 판별 */}
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goalText}>{itemData.item.text}</Text>
+              </View>
+            );
+          }}
+          keyExtractor={(item,index) => {
+            return item.id;
+          }}
+          alwaysBounceVertical={false}
+        />
+        {/* </ScrollView> */}
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  goalText: {
+    color: "white",
+  },
+});
