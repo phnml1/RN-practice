4e7964c2b51d9d826df3e60272de1ea841d37a69
lee juyoung <juyung0903@naver.com>
2025-01-16 19:05:34 +0900

    chore: useLayout에 관한 주석

--- foodApp/app/screens/MealsOverViewScreen.tsx
+++ foodApp/app/screens/MealsOverViewScreen.tsx
@@ -11,7 +11,10 @@ function MealsOverViewScreen({ navigation }) {
   const displayMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(categoryId) >= 0;
   });
-
+  // DOM 업데이트 후, 브라우저가 화면을 그리기 전에 실행
+  // 레이아웃 측정, 동기적으로 DOM 조작
+  // 동기적으로 실행되므로 성능에 영향을 줄 수 있음
+  // 화면이 업데이트된 이후에 타이틀도 바뀌므로 useLayoutEffect를 사용했음
   useLayoutEffect(() => {
     const categoryTitle = CATEGORIES!.find(
       (category) => category.id === categoryId
