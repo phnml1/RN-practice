ea58b28be55a265ea450739d84e04e565fd95544
lee juyoung <juyung0903@naver.com>
2025-01-20 18:06:54 +0900

    auth login & signup

--- auth/app/screens/LoginScreen.jsx
+++ auth/app/screens/LoginScreen.jsx
@@ -1,6 +1,23 @@
+import {useState} from 'react';
 import AuthContent from "../component/auth/AuthContent";
+import LoadingOverlay from '../component/ui/LoadingOverlay';
+import {login} from '../util/auth';
+import { Alert } from 'react-native';
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isAuthentcating, setIsAuthenticating] = useState();
+  async function loginHandler({email,password}) {
+    setIsAuthenticating(true);
+    try {
+    await login(email,password);
+    } catch(error) {
+      Alert.alert('');
+    }
+    setIsAuthenticating(false);
+  }
+  if (isAuthentcating) {
+    return <LoadingOverlay message="Logging you in..." />
+  }
+  return <AuthContent isLogin onAuthenticate={loginHandler}/>;
 }
 
 export default LoginScreen;
--- auth/app/screens/SignupScreen.jsx
+++ auth/app/screens/SignupScreen.jsx
@@ -1,6 +1,22 @@
 import AuthContent from "../component/auth/AuthContent";
+import LoadingOverlay from '../component/ui/LoadingOverlay';
+import {createUser} from '../util/auth';
+import { useState } from "react";
 function SignupScreen() {
-  return <AuthContent />;
+  const [isAuthentcating, setIsAuthenticating] = useState(false);
+  async function signupHandler({email,password}) {
+    setIsAuthenticating(true);
+    try {
+      await createUser(email,password);
+    } catch(error) {
+      Alert.alert('Authentication failed', 'Could not create user, please check your credentials or try again later!');
+    }
+    setIsAuthenticating(false);
+  }
+  if (isAuthentcating) {
+    return <LoadingOverlay message="Creating user..." />
+  }
+  return <AuthContent onAuthenticate={signupHandler}/>;
 }
 
 export default SignupScreen;
